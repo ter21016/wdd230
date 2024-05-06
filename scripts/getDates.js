b881886355ab16ba2cffc56ed2a89d05db4f8486
ter21016 <blackblue58@hotmail.com>
2024-05-06 15:08:27 -0700

    add normalize

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,36 +1,5 @@
 
 
-/*document.addEventListener("DOMContentLoaded", function () {
-    // Get current year and set it in the footer
-    const currentYear = new Date().getFullYear();
-    document.getElementById("currentYear").textContent = currentYear;
-
-    // Get last modified date and set it in the footer
-    const lastModifiedDate = document.lastModified;
-    document.getElementById("lastModified").textContent = "Last Modified: " + lastModifiedDate;
-
-    const hamburgerElement = document.querySelector('#myButton');
-    const navElement = document.querySelector('#animateme');
-
-    hamburgerElement.addEventListener('click', () => {
-        navElement.classList.toggle('open');
-        hamburgerElement.classList.toggle('open');
-    });
-
-
-  
-     
-    
-   const button= document.querySelector('#darkBtn');
-    
-    
-    darkBtn.addEventListener('click', () => {
-            mainElement.classList.toggle('dark-mode');
-    });
-    
-    
-});*/
-
 document.addEventListener("DOMContentLoaded", function () {
     // Get current year and set it in the footer
     const currentYear = new Date().getFullYear();
