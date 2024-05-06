431c02ae2c09c66d220ef1906651f2e95df85d0b
ter21016 <blackblue58@hotmail.com>
2024-05-06 14:48:18 -0700

    add dark mode

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,36 @@
+
+
+/*document.addEventListener("DOMContentLoaded", function () {
+    // Get current year and set it in the footer
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").textContent = currentYear;
+
+    // Get last modified date and set it in the footer
+    const lastModifiedDate = document.lastModified;
+    document.getElementById("lastModified").textContent = "Last Modified: " + lastModifiedDate;
+
+    const hamburgerElement = document.querySelector('#myButton');
+    const navElement = document.querySelector('#animateme');
+
+    hamburgerElement.addEventListener('click', () => {
+        navElement.classList.toggle('open');
+        hamburgerElement.classList.toggle('open');
+    });
+
+
+  
+     
+    
+   const button= document.querySelector('#darkBtn');
+    
+    
+    darkBtn.addEventListener('click', () => {
+            mainElement.classList.toggle('dark-mode');
+    });
+    
+    
+});*/
+
 document.addEventListener("DOMContentLoaded", function () {
     // Get current year and set it in the footer
     const currentYear = new Date().getFullYear();
@@ -6,14 +39,18 @@ document.addEventListener("DOMContentLoaded", function () {
     // Get last modified date and set it in the footer
     const lastModifiedDate = document.lastModified;
     document.getElementById("lastModified").textContent = "Last Modified: " + lastModifiedDate;
-});
 
+    const hamburgerElement = document.querySelector('#myButton');
+    const navElement = document.querySelector('#animateme');
+    const button = document.getElementById("darkMode");
+    const mainElement = document.querySelector('body');
 
-const hamburgerElement = document.querySelector('#myButton');
-const navElement = document.querySelector('#animateme');
+    hamburgerElement.addEventListener('click', () => {
+        navElement.classList.toggle('open');
+        hamburgerElement.classList.toggle('open');
+    });
 
-hamburgerElement.addEventListener('click', () =>{
-    navElement.classList.toggle('open');
-    hamburgerElement.classList.toggle('open');
-
+    button.addEventListener('click', () => {
+        mainElement.classList.toggle('darkMain');
+    });
 });
