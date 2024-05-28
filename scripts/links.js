89609fc2892d491ec872e0f8406c7235c33946d2
ter21016 <blackblue58@hotmail.com>
2024-05-28 09:35:25 -0700

    add js

--- scripts/links.js
+++ scripts/links.js
@@ -3,7 +3,7 @@
 const linkBox = document.getElementById('link-box');
 
 const baseUrl = 'https://github.com/ter21016/wdd230';
-const linksUrl = 'https://github.com/ter21016/wdd230/blob/main/scripts/links.js';
+const linksUrl = 'https://github.com/ter21016/wdd230/blob/main/data/links.json';
 
 
 async function getLinks() {
