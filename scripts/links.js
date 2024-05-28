8bb1cfbdf9aae6ddb7021e5fbcaeb436e3de9479
ter21016 <blackblue58@hotmail.com>
2024-05-28 11:19:09 -0700

    add js

--- scripts/links.js
+++ scripts/links.js
@@ -3,35 +3,44 @@
 const linkBox = document.getElementById('link-box');
 
 const baseUrl = 'https://github.com/ter21016/wdd230';
-const linksUrl = 'https://github.com/ter21016/wdd230/blob/main/data/links.json';
+const linksUrl = 'data/links.json';
 
 
 async function getLinks() {
-    const response = await fetch(linksURL);
+    const response = await fetch(linksUrl);
     const data = await response.json();
     console.log(data);
     displayLinks(data.weeks);
   }
-  
-  getLinks();
 
-const displayLinks = function(data) {
-    for (let index = 0; index < data.length; index++) {
-        let listEl = document.createElement('li');
 
-        listEl.innerHTML = `${data[index].week}: `;
-        data[index].links.forEach((link, i) => {
-            let linkEl = document.createElement('a');
-            linkEl.href = link.url;
-            linkEl.title = link.title;
-            linkEl.innerHTML = link.title;
-            linkEl.target = '_blank';
 
-            listEl.appendChild(linkEl)
-        });
-
-        linkBox.appendChild(listEl);
+const displayLinks = function(weeks) {
+    for (let i = 0; i < weeks.length; i++) {
+        const week = weeks[i];
+        const listElement = document.createElement('li');
+        listElement.innerHTML = `${week.week}: `;
+        
+        const links = week.links;
+        for (let j = 0; j < links.length; j++) {
+            const link = links[j];
+            const linkElement = document.createElement('a');
+            linkElement.href = link.url;
+            linkElement.textContent = link.title;
+            linkElement.target = '_blank';
+            listElement.appendChild(linkElement);
+            
+            // Adding separator if not the last link
+            if (j !== links.length - 1) {
+                const separator = document.createElement('span');
+                separator.textContent = ' | ';
+                listElement.appendChild(separator);
+            }
+        }
+        
+        linkBox.appendChild(listElement);
     }
 };
+
 
 getLinks();
