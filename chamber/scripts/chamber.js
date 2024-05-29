0dc1b569b2ef7afeb7231fac9f60c1e967b30c83
ter21016 <blackblue58@hotmail.com>
2024-05-28 19:08:31 -0700

    add json

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -89,3 +89,74 @@ todayElement.textContent = today;
 christmasElement.textContent = christmasDate.getTime();
 christmasDateElement.textContent = christmasDate;
 daysElement.textContent = `${daysleft.toFixed(0)} days`;
+
+// Directory//
+
+
+
+
+/////////////// API Grid ///////////////////
+/* GitHub link for directory: 'https://raw.githubusercontent.com/klittler-byui/wdd230/main/04-Chamber/04js/directorylist.json' */
+const baseUrl = "https://github.com/ter21016/wdd230/blob/main/chamber/index.html";
+const requestURL = "data/members.json"  ;
+
+const Mcards = document.querySelector('.Mcards');
+
+fetch(requestURL)
+.then(function (response) {
+  return response.json();
+})
+.then(function (jsonObject) {
+  console.table(jsonObject);
+  const members = jsonObject['members'];
+  members.forEach(displayMembers);
+});
+
+function displayMembers (member) {
+  let Mcard = document.createElement('section');
+  let name = document.createElement('name');
+  let logo = document.createElement('img');
+  let address = document.createElement('span');
+  let phone = document.createElement('span');
+  let web = document.createElement('a');
+
+  name.textContent = `${member.name}`;
+  address.textContent = `${member.address}`;
+  phone.textContent = `${member.phone}`;
+  web.textContent = `${member.web}`;
+
+  logo.setAttribute('src', member.logoImg);
+  logo.setAttribute('alt', `${member.name} logo`);
+  logo.setAttribute('loading', 'lazy');
+  web.setAttribute("href", member.web);
+  web.setAttribute("target", "_blank");
+
+  Mcard.appendChild(logo);
+  Mcard.appendChild(name);
+  Mcard.appendChild(address);
+  Mcard.appendChild(phone);
+  Mcard.appendChild(web);
+
+  document.querySelector('div.Mcards').appendChild(Mcard);
+}
+
+
+//////// Grid View Toggle Function ///////////
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".Mcards");
+
+
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
